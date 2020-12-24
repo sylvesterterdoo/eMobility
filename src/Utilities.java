adc7dd50570267579940d62e2edd489206465e13
Sylvester <sylvesterterdoo@gmail.com>
2020-12-24 16:17:46 +0100

    [ Utilities ] utilities class

--- src/Utilities.java
+++ src/Utilities.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Utilities {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Utilities {
+
+    public static void addExceptions(Exception exception, Map<ExceptionType, List<Exception>> exceptions) {
+        exceptions.computeIfAbsent(exception.exceptionType, k -> {
+            List<Exception> newExceptions = new ArrayList<>();
+            newExceptions.add(exception);
+            return newExceptions;
+        });
+
+        exceptions.computeIfPresent(exception.exceptionType, (k, v) -> {
+            List<Exception> savedExceptions = exceptions.get(k);
+            savedExceptions.add(exception);
+            return savedExceptions;
+        });
+    }
 }
