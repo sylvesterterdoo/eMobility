aba4fd19d41a8a7a5b757c8934d3022def942306
Sylvester <sylvesterterdoo@gmail.com>
2020-12-24 16:16:13 +0100

    [ Exception ] Object that models exceptions

--- src/Exception.java
+++ src/Exception.java
@@ -1,13 +1,38 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 enum ExceptionType {
     OPEN,
     CLOSE
 }
 
-public class Exceptions {
+public class Exception {
     LocalDate fromDate;
     LocalDate toDate;
     TimeSpan timeSpan;
     ExceptionType exceptionType;
+
+    public Exception(LocalDate fromDate, LocalDate toDate, TimeSpan timeSpan, ExceptionType exceptionType) {
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.timeSpan = timeSpan;
+        this.exceptionType = exceptionType;
+    }
+
+    public boolean isDateBetween(LocalDate localDate) {
+        if ((localDate.compareTo(fromDate)) >= 0 &&
+                (localDate.compareTo(toDate) <= 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isTimeBetween(LocalTime localTime) {
+        if ((localTime.compareTo(timeSpan.getFrom())) >= 0 &&
+                (localTime.compareTo(timeSpan.getTo()) < 0)) {
+            return true;
+        }
+        return false;
+    }
+
 }
