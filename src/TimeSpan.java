1d7c1e0a15c97830f58bc46d5b624b498a54af2f
Sylvester <sylvesterterdoo@gmail.com>
2020-12-24 16:17:17 +0100

    [ TimeSpan ] Object that models TimeSpan

--- src/TimeSpan.java
+++ src/TimeSpan.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class TimeSpan {
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class TimeSpan {
+    private LocalTime from;
+    private LocalTime to;
+
+    public TimeSpan(LocalTime from, LocalTime to) {
+        this.from = from;
+        this.to = to;
+    }
+
+
+    public LocalTime getFrom() {
+        return from;
+    }
+
+    public void setFrom(LocalTime from) {
+        this.from = from;
+    }
+
+    public LocalTime getTo() {
+        return to;
+    }
+
+    public void setTo(LocalTime to) {
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeSpan{" +
+                "from=" + from +
+                ", to=" + to +
+                '}';
+    }
 }
