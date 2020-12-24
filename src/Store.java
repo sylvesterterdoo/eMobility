0cd7594cad3e9fc3b5b8c6d17ed0caacb31117ed
Sylvester <sylvesterterdoo@gmail.com>
2020-12-24 16:36:08 +0100

    [ Store, Tenant ] updated store and tenant

--- src/Store.java
+++ src/Store.java
@@ -1,4 +1,6 @@
 import java.time.DayOfWeek;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -6,17 +8,34 @@ public class Store {
     private static int instanceCounter = 0;
 
     private int storeNumber;
+    private Tenant tenant;
     private List<ChargingStation> chargingStations;
     private Map<ExceptionType, List<Exception>> exceptions;
     private Map<DayOfWeek, List<TimeSpan>> openingTimeSchedule;
 
-    public Store() {
+    public Store(Tenant tenant) {
+        this.tenant = tenant;
         this.storeNumber = instanceCounter;
+
+        Map<ExceptionType, List<Exception>> exceptions = new HashMap<>();
+        exceptions.put(ExceptionType.OPEN, new LinkedList<>());
+        exceptions.put(ExceptionType.CLOSE, new LinkedList<>());
+
+        this.exceptions = exceptions;
+
         instanceCounter += 1;
     }
 
     public int getStoreNumber() {
         return storeNumber;
+    }
+
+    public Tenant getTenant() {
+        return tenant;
+    }
+
+    public Map<ExceptionType, List<Exception>> getExceptions() {
+        return exceptions;
     }
 
     public void addException(Exception exception) {
--- src/Tenant.java
+++ src/Tenant.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Tenant {
     String name;
@@ -10,10 +8,20 @@ public class Tenant {
     public Tenant(String name) {
         this.name = name;
         stores = new ArrayList<>();
+
+        Map<ExceptionType, List<Exception>> exceptions = new HashMap<>();
+        exceptions.put(ExceptionType.OPEN, new LinkedList<>());
+        exceptions.put(ExceptionType.CLOSE, new LinkedList<>());
+
+        this.exceptions = exceptions;
     }
 
     public void addStore(Store store) {
         this.stores.add(store);
+    }
+
+    public Map<ExceptionType, List<Exception>> getExceptions() {
+        return exceptions;
     }
 
     public void addException(Exception exception) {
