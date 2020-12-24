4dc3d5cfb8302c1b280ac9ead64b175e68d279d3
Sylvester <sylvesterterdoo@gmail.com>
2020-12-24 16:16:59 +0100

    [ Tenant ] Object that models tenant

--- src/Tenant.java
+++ src/Tenant.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Tenant {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Tenant {
+    String name;
+    List<Store> stores;
+    private Map<ExceptionType, List<Exception>> exceptions;
+
+    public Tenant(String name) {
+        this.name = name;
+        stores = new ArrayList<>();
+    }
+
+    public void addStore(Store store) {
+        this.stores.add(store);
+    }
+
+    public void addException(Exception exception) {
+        Utilities.addExceptions(exception, this.exceptions);
+    }
 }
