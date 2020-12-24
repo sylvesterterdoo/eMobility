06147cfe6d3e3e39af17eeabef873db158e8b691
Sylvester <sylvesterterdoo@gmail.com>
2020-12-24 16:16:44 +0100

    [ Store ] Object that models store

--- src/Store.java
+++ src/Store.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Store {
+import java.time.DayOfWeek;
+import java.util.List;
+import java.util.Map;
+
+public class Store {
+    private static int instanceCounter = 0;
+
+    private int storeNumber;
+    private List<ChargingStation> chargingStations;
+    private Map<ExceptionType, List<Exception>> exceptions;
+    private Map<DayOfWeek, List<TimeSpan>> openingTimeSchedule;
+
+    public Store() {
+        this.storeNumber = instanceCounter;
+        instanceCounter += 1;
+    }
+
+    public int getStoreNumber() {
+        return storeNumber;
+    }
+
+    public void addException(Exception exception) {
+        Utilities.addExceptions(exception, this.exceptions);
+    }
 }
