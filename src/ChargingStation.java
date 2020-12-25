674f035cf6f6f8d027bdaf021ffbe92dd59be8f0
Sylvester <sylvesterterdoo@gmail.com>
2020-12-25 11:25:08 +0100

    [ ChargingStation ] added next open closed status change

--- src/ChargingStation.java
+++ src/ChargingStation.java
@@ -1,8 +1,5 @@
 import java.sql.Timestamp;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.util.*;
 
 enum StationType {
@@ -70,6 +67,45 @@ public class ChargingStation {
         }
     }
 
+    public Timestamp nextOpenClosedStatusChange(Timestamp timestamp) {
+        LocalDateTime dateTime = timestamp.toLocalDateTime();
+
+        Timestamp result = dateStatusChange(exceptions.get(ExceptionType.CLOSE), dateTime);
+        if (result == null) {
+            result = dateStatusChange(exceptions.get(ExceptionType.OPEN), dateTime);
+        }
+
+        if (result == null) {
+            result = dateStatusChange(store.getExceptions().get(ExceptionType.CLOSE), dateTime);
+        }
+        if (result == null) {
+            result = dateStatusChange(store.getExceptions().get(ExceptionType.OPEN), dateTime);
+        }
+
+        if (result == null) {
+            result = dateStatusChange(store.getTenant().getExceptions().get(ExceptionType.CLOSE), dateTime);
+        }
+        if (result == null) {
+            result = dateStatusChange(store.getTenant().getExceptions().get(ExceptionType.OPEN), dateTime);
+        }
+        if (result == null) {
+            result = sheduleStatusChange(dateTime);
+        }
+
+        return result;
+
+    }
+
+
+    /* Helper method that convert a given LocalTime and Localdate to Timestamp */
+    private Timestamp convertLocalTimeToTimestamp(LocalTime time, LocalDateTime dateTime) {
+        Instant instant = time.atDate(dateTime.toLocalDate())
+                .atZone(ZoneId.systemDefault()).toInstant();
+        return new Timestamp(Date.from(instant).getTime());
+
+    }
+
+
     /** Helper method that checks the opening times from a station schedule
      * given a LocalTime and DayOfWeek */
     private boolean isOpeningHoursInSchedule(LocalTime checkTime, DayOfWeek weekDay) {
@@ -85,7 +121,100 @@ public class ChargingStation {
         });
     }
 
+    private Timestamp dateStatusChange(List<Exception> exceptions, LocalDateTime checkDateTime) {
 
+        LocalDateTime nextDateTime = checkDateTime.plusDays(1);
+
+        LocalDate checkDate = checkDateTime.toLocalDate();
+        LocalTime checkTime = checkDateTime.toLocalTime();
+        for(Exception exception: exceptions) {  // checkDate.equals(nextDateTime.toLocalDate())
+            if (exception.isDateBetween(checkDate) ) {
+                return getNextTimeFrom(exception, checkDateTime);
+            } else if (checkDate.equals(nextDateTime.toLocalDate())) {
+                // if it's the to return the nextday (check the time is not after the normal)
+                return getNextTimeFrom(exception, checkDateTime);
+            }
+        }
+        return null;
+    }
+
+
+    private Timestamp getNextTimeFrom(Exception exception, LocalDateTime dateTime) {
+        TimeSpan currentTimeSpan = exception.timeSpan;
+        LocalTime checkTime = dateTime.toLocalTime();
+
+        Timestamp timestamp = null;
+        if (currentTimeSpan.getFrom().compareTo(checkTime) > 0 && !isOpeningHours(currentTimeSpan, checkTime)) {
+            // before opening time
+            timestamp = convertLocalTimeToTimestamp(currentTimeSpan.getFrom(), dateTime);
+
+        } else if (isOpeningHours(currentTimeSpan, checkTime)) {
+            // during opening time
+            timestamp =  convertLocalTimeToTimestamp(currentTimeSpan.getTo(), exception.toDate.atTime(checkTime));
+
+        }
+        return timestamp;
+    }
+
+    public Timestamp sheduleStatusChange(LocalDateTime dateTime) {
+        DayOfWeek checkDay = dateTime.getDayOfWeek();
+        LocalTime checkTime= dateTime.toLocalTime();
+
+        Timestamp timestamp = null;
+        int i = 0;
+        List<TimeSpan> timeSpans = openingTimeSchedule.get(checkDay);
+
+        while (i < timeSpans.size()) {
+            TimeSpan currentTimeSpan = timeSpans.get(i);
+
+
+            if (currentTimeSpan.getFrom().compareTo(checkTime) > 0 && !isOpeningHours(currentTimeSpan, checkTime)) {
+                // before opening time
+                timestamp = convertLocalTimeToTimestamp(currentTimeSpan.getFrom(), dateTime);
+                break;
+
+            } else if (isOpeningHours(currentTimeSpan, checkTime)) {
+                // during opening time
+                timestamp =  convertLocalTimeToTimestamp(currentTimeSpan.getTo(), dateTime);
+                break;
+
+            } else if (currentTimeSpan.getTo().compareTo(checkTime) < 0 ) {
+                // after opening time
+                if ((i+1) < timeSpans.size()) {
+                    // get next timeSpan from same day
+                    timestamp = convertLocalTimeToTimestamp(timeSpans.get(i+1).getFrom(), dateTime);
+                    break;
+                } else {
+                    // get nextDay timeSpan or next monday if it is saturday
+                    int daysToAdd = checkDay.equals(DayOfWeek.SATURDAY) ? 2 : 1;
+                    checkDay = checkDay.plus(daysToAdd);
+                    timeSpans = openingTimeSchedule.get(checkDay);
+                    timestamp = convertLocalTimeToTimestamp(timeSpans.get(0).getFrom(), dateTime.plusDays(daysToAdd));
+                    break;
+                }
+            }
+            // next timeSpan
+            i += 1;
+        }
+
+        return timestamp;
+    }
+
+
+    /* Helper method that checks the openingHours of a station given a TimeSpan and LocalTime */
+    private boolean isOpeningHours(TimeSpan timeSpan, LocalTime checkTime) {
+        return checkTime.query(temporal -> {
+            LocalTime from = timeSpan.getFrom();
+            LocalTime to = timeSpan.getTo();
+            if ((checkTime.compareTo(from) >= 0) && (checkTime.compareTo(to) < 0)) {
+                return true;
+            }
+            return false;
+        });
+    }
+
+
+    // move method to exception class
     private boolean isExpectionsBetween(List<Exception> exceptions, LocalDateTime dateTime) {
         LocalDate date = dateTime.toLocalDate();
         LocalTime time = dateTime.toLocalTime();
